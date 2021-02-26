bf7acd1769e44f7eadfad2772f3d185ffd981d0f
parzivalClaus <claudio@kaizenfotoprodutos.com.br>
2021-02-26 13:38:54 -0300

    nlw4 finished

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,73 @@
+import { count } from 'console';
+import { useState, useEffect, useContext } from 'react';
+import { CountdownContext } from '../contexts/CountdownContexts';
+import styles from '../styles/components/Countdown.module.css';
+
+export function Countdown() {
+    const { 
+        minutes,
+        seconds,
+        hasFinished,
+        isActive,
+        startCountdown,
+        resetCountdown
+    } = useContext(CountdownContext);
+
+    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+ 
+    return (
+        <div>
+        <div className={styles.countdownContainer}>
+            <div>
+                <span>{minuteLeft}</span>
+                <span>{minuteRight}</span>
+            </div>
+            <span>:</span>
+            <div>
+                <span>{secondLeft}</span>
+                <span>{secondRight}</span>
+            </div>
+        </div>
+
+        { hasFinished ? (
+             <button 
+             disabled
+             className={styles.countdownButton}
+             >
+                Ciclo encerrado
+                <img src="icons/confirm.svg" alt="Level"/>
+                 </button>
+                 
+        ) : (
+            <>
+            { isActive ? (
+            <button 
+        type="button" 
+        className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+        onClick={resetCountdown}
+        >
+           Abandonar ciclo
+           <img src="icons/close.svg" alt="Level"/>
+            </button>
+        ) : (
+            <button 
+            type="button" 
+            className={styles.countdownButton}
+            onClick={startCountdown}
+            >
+                Iniciar um ciclo
+                <img src="icons/play.svg" alt="Level"/>
+            </button>
+        )}
+            </>
+        ) }
+
+        
+
+        
+
+           
+        </div>
+    );
+}
